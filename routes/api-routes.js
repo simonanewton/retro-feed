05963aba2bb0ab1ababcc83ed44d7af859b40572
Terry C <terrycetoute@gmail.com>
2020-06-04 13:00:07 -0400

    sign up working except for the password

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,5 +14,20 @@ module.exports = (app) => {
         });
         res.json(post);
     });
-    
+
+    app.get("/api/signup", async (req, res) => {
+        const users = await db.newUser.findAll({});
+        res.json(users);
+    });
+
+    app.post("/api/signup", async (req, res) => {
+        const newSignUp = await db.newUser.create({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        });
+        console.log(newSignUp.dataValues)
+        res.json(newSignUp.dataValues);
+
+    });
 }
