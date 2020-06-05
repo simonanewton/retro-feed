127019e4e2fe88daecf9e6ad93141a69eb95f8a3
Logan Hemphill <logan.hemphill86@gmail.com>
2020-06-05 11:39:46 -0400

    added profile page and route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -42,4 +42,22 @@ module.exports = (app) => {
         // render the posts.handlebars page with the posts
         res.render("posts", { posts: posts });
     });
+
+    app.get("/profile", async (req, res) => {
+        // create an array of all posts in the database
+        let posts = await db.Post.findAll({});
+
+        // isolate the post data from the array
+        posts = posts.map(post => post.dataValues);
+
+        // modify the createdAt variable with moment to show how long ago the post was created
+        posts.map(post => post.createdAt = moment(post.createdAt, 'YYYY-MM-DDTHH:mm:ss.000Z').fromNow());
+
+        // reverse the array to show the most recent posts first
+        posts.reverse();
+
+        // render the posts.handlebars page with the posts
+        res.render("profile", { posts: posts });
+    });
+
 }
