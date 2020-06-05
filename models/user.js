24f0cf22adf96cbbf763a62a562c2da5d3a043ef
Terry C <terrycetoute@gmail.com>
2020-06-05 00:39:34 -0400

    user js file updated with validation

--- models/user.js
+++ models/user.js
@@ -2,11 +2,30 @@ const bcrypt = require("bcryptjs");
 
 module.exports = (sequelize, DataTypes) => {
     // Create newUser database with User rows
-    let User = sequelize.define("newUser", {
-        email: DataTypes.STRING,
-        username: DataTypes.STRING,
-        password: DataTypes.STRING
+    var User = sequelize.define("newUser", {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        username: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            unique: true,
+        },
+        password: {
+            type: DataTypes.STRING(64),
+            allowNull: false,
+            is: /^[0-9a-f]{64}$/i
+        }
     });
+    // This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+    User.prototype.validPassword = (password) => {
+        return bcrypt.compareSync(password, this.password);
+    };
 
     // Hash user's password beforeCreate into newUsers database 
     User.addHook("beforeCreate", (user) => {
