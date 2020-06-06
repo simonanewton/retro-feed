45bd3d90fe85584410b6b262ee2ea73cc39a5d35
Logan Hemphill <logan.hemphill86@gmail.com>
2020-06-06 08:39:09 -0400

    added responsive photo to profile

--- public/js/main.js
+++ public/js/main.js
@@ -48,4 +48,13 @@ $(document).ready(() => {
         // reload the window
         location.reload();
     });
+
+    $('.thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    ); 
 });
