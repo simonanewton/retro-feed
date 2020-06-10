57d819bad00a974c0a4e7e71ab65e38cd218dd5d
Connie Tran <connietrandev@gmail.com>
2020-06-09 22:17:33 -0400

    Added functionality to display unique user profile and URL route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,6 +3,7 @@ const moment = require("moment");
 const authenticate = require("../config/authenticate");
 
 module.exports = (app) => {
+    /* Log in / Signup ------------------------------------- */
     app.get("/", async (req, res) => {
         // If the user already has an account send them to the posts page
         if (req.user) res.redirect("/posts");
@@ -35,6 +36,7 @@ module.exports = (app) => {
         res.redirect("/");
     });
 
+    /* Posts Feed ------------------------------------- */
     app.get("/posts", authenticate, async (req, res) => {
         // create an array of all posts in the database
         let posts = await db.Post.findAll({});
@@ -49,12 +51,25 @@ module.exports = (app) => {
         posts.reverse();
 
         // render the posts.handlebars page with the posts
-        res.render("posts", { posts: posts });
+        // pass current username to generate unique URL for Profile link
+        res.render("posts", {
+            posts: posts,
+            username: req.user.username
+        });
     });
 
-    app.get("/profile", async (req, res) => {
-        // create an array of all posts in the database
-        let posts = await db.Post.findAll({});
+    /* User Profile ------------------------------------- */
+    app.get("/:username", authenticate, async (req, res) => {
+        // use username parameter for WHERE queries
+        let currentUser = req.params.username;
+
+        /* get User Posts */
+        // create an array of all posts in the database WHERE username = current user
+        let posts = await db.Post.findAll({
+            where: {
+                username: currentUser
+            }
+        });
 
         // isolate the post data from the array
         posts = posts.map(post => post.dataValues);
@@ -65,8 +80,19 @@ module.exports = (app) => {
         // reverse the array to show the most recent posts first
         posts.reverse();
 
-        // render the posts.handlebars page with the posts
-        res.render("profile", { posts: posts });
+        /* get User Info */
+        let userInfo = await db.User.findAll({
+            where: {
+                username: currentUser
+            }
+        });
+
+        // render the posts.handlebars page with posts and user info
+        res.render("profile", {
+            posts: posts,
+            displayName: userInfo[0].dataValues.displayName,
+            username: userInfo[0].dataValues.username
+        });
     });
 
 }
